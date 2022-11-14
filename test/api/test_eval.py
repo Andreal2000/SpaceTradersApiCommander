c55753cd94ebf2312016ad3e90cfa5a9501c6f0d
Andrea Longoni <andrealong2000@gmail.com>
2022-11-14 18:32:57 +0100

    Update test_eval

--- test/api/test_eval.py
+++ test/api/test_eval.py
@@ -1,44 +1,37 @@
 import json
 import requests
 
-test = {
-    "http_method": "GET",
-    "endpoint": "my/account/",  # "my/account/{shipId}"
-    "url_params": [],
-    "body_params": [],
-    "method_name": "getAccountInfo",
-    "method_docs": "Get information on your account",
-}
 
-URL = "https://api.spacetraders.io/"
-
-# def readjson(file):
-#     generate_func(data)
+def generate_class(file):
+    with open(file) as f:
+        data = json.load(f)
+        endpoints = {e["method_name"]: generate_func(e) for e in data["endpoints"]}
+        def init(self, token): self.token = token
+        class_dict = dict({"__init__": init, "url": data["domain"], "token": ""}, **endpoints)
+        return type("client", (), class_dict)
 
 
 def generate_func(data):
     template = """lambda self {method_params}: requests.{http_method}(
         url=self.url + f"{endpoint}", 
         headers={{'Authorization': 'Bearer ' + self.token,'Content-Type': 'application/json'}},
-        params={body_params})"""
+        params={body_params}).json()"""
 
     method_params_list = data["url_params"]+data["body_params"]
-    method_params = "" if method_params_list is [] else "".join(
-        [f", {x}" for x in method_params_list])
-    body_params = "None" if data["body_params"] == [
-    ] else "{" + ", ".join([f"'{x}': {x}" for x in data["body_params"]]) + "}"
+    method_params = "" if method_params_list is [] else "".join([f", {x}" for x in method_params_list])
+    body_params = "None" if data["body_params"] == [] else "{" + ", ".join([f"'{x}': {x}" for x in data["body_params"]]) + "}"
 
-    func = template.format(
+    str_func = template.format(
         method_params=method_params,
         http_method=data["http_method"].lower(),
         endpoint=data["endpoint"],
         body_params=body_params)
 
-    return eval(func)
+    func = eval(str_func)
+    func.__doc__ = data["method_docs"]
+    return func
 
 
-myclass = type("myclass", (), {"url": "https://api.spacetraders.io/",
-               "token": "", test["method_name"]: generate_func(test)})
-
-m = myclass()
-print(m.getAccountInfo().text)
+myclass = generate_class("src\\api\endpointsV1.json")
+m = myclass("token")
+print(m.getAccountInfo())
