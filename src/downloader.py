9831771aa27f41e10606c3c3ad4364bbb8600fc4
Andrea Longoni <andrealong2000@gmail.com>
2022-12-15 21:12:17 +0100

    Fix travelling_salesman_problem function

--- src/downloader.py
+++ src/downloader.py
@@ -1,15 +1,4 @@
-# TODO
-# download goods if utils/goods is empty
-# download systems and location if utils/systems is empty [or not equal to the server]
-# generate the Travelling salesman problem solution and save on a file
-# leggendo il file di prima fa viaggiare la nave per i pianeti e salva i marketplace info in utils/marketplaces
-#   (combrare ad ogni pianeta la benzina se a 1 o serbatoio quasi vuoto)
-# generare un grafo in base a pianeti e cosa vendono e salvarlo in utils
-#
-# 
-
 import json
-import math
 from api import client
 
 SYSTEMS = ("OE", "XV", "ZY1", "NA7")
@@ -76,4 +65,3 @@ def get_systems(*systems):
         except:
             result[s] = download_systems(s)[s]
     return result
-
--- src/explorer.py
+++ src/explorer.py
@@ -1,5 +1,7 @@
-from downloader import  *
+from downloader import *
 from itertools import permutations
+import math
+
 
 def calculate_distance(x1, y1, x2, y2):
     return round(math.sqrt((x2-x1)**2 + (y2-y1)**2))
@@ -20,23 +22,59 @@ def calculate_fuel_cost(distance, fuel_efficiency, docking_efficiency, origin_lo
 def get_systems_matrix(*systems):
     if len(systems) == 0:
         systems = SYSTEMS
-    return {s : [ [ h["symbol"] for h in l ] , [ [ calculate_distance(a["x"],a["y"],b["x"],b["y"]) for b in l ] for a in l ] ] for s , l in get_systems(*systems).items()}
+    return {s: [[h["symbol"] for h in l], [[calculate_distance(a["x"], a["y"], b["x"], b["y"]) for b in l] for a in l]] for s, l in get_systems(*systems).items()}
 
 
 def travelling_salesman_problem(*systems):
     if len(systems) == 0:
         systems = SYSTEMS
-    
+
     def inner(system):
-        header = system[0] 
+        header = system[0]
         system = system[1]
-        n = len(system)
         m = min([(sum([system[p[i]][p[(i+1) % len(p)]] for i in range(len(p))]), p) for p in permutations(range(len(system))) if p < p[::-1]])
         return [header[i] for i in m[1]]
-    
-    for k,v in get_systems_matrix(*systems).items():
-        with open(f"utils/systems/tsp/TSP_{k}.json", "w") as file:
-            print(k)
-            file.write(json.dumps({k: inner(v)}, indent=4))
 
-travelling_salesman_problem()
+    def inner_apx(system):
+        header = system[0]
+        system = system[1]
+        out = []
+        rm = []
+        nh = {}
+
+        for h in header:
+            if len(h.split("-")) == 3 and h.split("-")[1] != "W":
+                p = "-".join(h.split("-")[:2])
+                if nh.get(p) is None:
+                    nh[p] = [h]
+                else:
+                    nh[p] = nh[p] + [h]
+                    rm += [header.index(h)]
+            else:
+                nh[h] = []
+
+        for x in reversed(range(len(system))):
+            if x in rm:
+                del system[x]
+            else:
+                for y in reversed(rm):
+                    del system[x][y]
+        
+        m = min([(sum([system[p[i]][p[(i+1) % len(p)]] for i in range(len(p))]), p) for p in permutations(range(len(system))) if p < p[::-1]])
+        # m = (569, (0, 1, 7, 10, 4, 5, 8, 6, 9, 3, 2)) # ZY1
+
+        k = list(nh.keys())
+        for i in m[1]:
+            out.append(k[i])
+            if nh.get(k[i]) is not None and len(nh.get(k[i])) > 0:
+                out.extend(nh[k[i]])
+        return out
+
+    for k, v in get_systems_matrix(*systems).items():
+        with open(f"utils/systems/tsp/TSP_{k}.json", "w") as file:
+            file.write(json.dumps({k: inner(v) if len(v[1]) <= 11 else inner_apx(v)}, indent=4))
+
+# TODO
+# leggendo il file di prima fa viaggiare la nave per i pianeti e salva i marketplace info in utils/marketplaces
+#   (combrare ad ogni pianeta la benzina se a 1 o serbatoio quasi vuoto)
+# generare un grafo in base a pianeti e cosa vendono e salvarlo in utils
